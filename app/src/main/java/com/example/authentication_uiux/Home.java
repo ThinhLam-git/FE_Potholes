ce92326d87a81bbe9afd96ce18d3b7e58053bb2b
Dat <tandat41202gmail.com>
2024-11-06 22:25:52 +0700

    Home_design

---
+++ app/src/main/java/com/example/authentication_uiux/Home.java
@@ -0,0 +1,37 @@
+package com.example.authentication_uiux;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_home);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Setting button listener
+        findViewById(R.id.setting_change).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start the setting activity
+                Intent intent = new Intent(Home.this, setting.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
