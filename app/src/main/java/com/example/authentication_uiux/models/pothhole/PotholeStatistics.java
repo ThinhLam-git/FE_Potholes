b414043776378534a2b58c3a0e7348eeb92f34e8
KhoaLe <khoahiro123@gmail.com>
2024-12-23 01:00:46 +0700

    upload: number total pothhole (from all user) to dashboard

---
+++ app/src/main/java/com/example/authentication_uiux/models/pothhole/PotholeStatistics.java
@@ -0,0 +1,40 @@
+package com.example.authentication_uiux.models.pothhole;
+
+public class PotholeStatistics {
+    private int totalPotholes;
+    private int resolvedPotholes;
+    private int reportedPotholes;
+    private int inProgressPotholes;
+
+    public int getTotalPotholes() {
+        return totalPotholes;
+    }
+
+    public void setTotalPotholes(int totalPotholes) {
+        this.totalPotholes = totalPotholes;
+    }
+
+    public int getResolvedPotholes() {
+        return resolvedPotholes;
+    }
+
+    public void setResolvedPotholes(int resolvedPotholes) {
+        this.resolvedPotholes = resolvedPotholes;
+    }
+
+    public int getReportedPotholes() {
+        return reportedPotholes;
+    }
+
+    public void setReportedPotholes(int reportedPotholes) {
+        this.reportedPotholes = reportedPotholes;
+    }
+
+    public int getInProgressPotholes() {
+        return inProgressPotholes;
+    }
+
+    public void setInProgressPotholes(int inProgressPotholes) {
+        this.inProgressPotholes = inProgressPotholes;
+    }
+}
